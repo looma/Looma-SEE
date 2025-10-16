c8461d5e42647c33cc81dbe074b62f6fc984c470
BeckettPorter <porterbeckett@gmail.com>
2025-10-16 08:55:15 -0700

    Update exam-tabs.tsx

--- nepali-exam-helper/app-code/components/exam-tabs.tsx
+++ nepali-exam-helper/app-code/components/exam-tabs.tsx
@@ -545,7 +545,6 @@ export function ExamTabs({ studentId, testId, onProgressUpdate, onShowResults, o
                             gapId: gapId,
                           }))
                       )
-                    )
                     }
                   }
                 })
