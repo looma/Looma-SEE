dcbb8b593ea9f162f82179d724b2521234d07ee1
BeckettPorter <porterbeckett@gmail.com>
2025-11-30 11:38:02 -0800

    Incremented version number

--- nepali-exam-helper/app-code/components/version-indicator.tsx
+++ nepali-exam-helper/app-code/components/version-indicator.tsx
@@ -10,7 +10,7 @@ export function VersionIndicator() {
           color: '#ffffff !important'
         } as React.CSSProperties}
       >
-        <span style={{ color: '#ffffff' }}>v1.0.8</span>
+        <span style={{ color: '#ffffff' }}>v1.0.9</span>
       </div>
     </div>
   )
